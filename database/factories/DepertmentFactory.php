44aa933bccaad457653f1759d2e6611503d734af
Abir4221 <70932003+Abir4221@users.noreply.github.com>
2020-11-20 22:54:56 -0800

    create department,employee,depertment factory,employee factory,depertment seeder,employee seeder,add employee

---
+++ database/factories/DepertmentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\depertment;
+use Faker\Generator as Faker;
+
+$factory->define(depertment::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
